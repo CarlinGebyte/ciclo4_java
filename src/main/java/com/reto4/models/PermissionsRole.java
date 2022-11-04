4b94713418db4cca216dad159e95fc3a5c1deab8
CarlinGebyte <krapulayt@gmail.com>
2022-11-04 16:54:53 -0500

    Add DBRef to PermissionsRole.java

--- src/main/java/com/reto4/models/PermissionsRole.java
+++ src/main/java/com/reto4/models/PermissionsRole.java
@@ -3,12 +3,15 @@ package com.reto4.models;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 
 @Data
 @NoArgsConstructor
 public class PermissionsRole {
     @Id
     private String _id;
+    @DBRef
     private Role role;
+    @DBRef
     private Permission permission;
 }
